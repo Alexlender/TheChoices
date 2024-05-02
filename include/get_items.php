996688df8367378aa6a9e5f8283b754965e9d3f9
Alexlender <sashacharugin@mail.ru>
2024-05-02 16:58:42 +0700

    123

--- include/get_items.php
+++ include/get_items.php
@@ -8,14 +8,14 @@ function get_items()
 
     $row = $result->fetch_assoc();
     $item1 = new item();
-    $item1->$name = $row["name"];
-    $item1->$image = $row["image"];
-    echo  print_r($item1);
+    $item1->name = $row["name"];
+    $item1->image = $row["image"];
+    echo  print_r($item1->name);
 
     $row = $result->fetch_assoc();
     $item2 = new item();
-    $item2->$name = $row["name"];
-    $item2->$image = $row["image"];
+    $item2->name = $row["name"];
+    $item2->image = $row["image"];
 
     return array($item1, $item2);
 }
