91041e6bf052f9d70ace7aa791798d58979b87ce
Alexlender <sashacharugin@mail.ru>
2024-05-02 16:14:33 +0700

    i

--- include/get_items.php
+++ include/get_items.php
@@ -4,7 +4,7 @@ require_once 'connect.php';
 
 $result = $connect->query("SELECT * FROM items order by RAND() LIMIT 2");
 
-while($row = mysql_fetch_array($result)) {
+while($row = mysqli_fetch_array($result)) {
     echo $row['column_name'];
     echo print_r($row);       
 }
