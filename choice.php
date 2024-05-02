f5f3efd2f743a00bc2f85e76bbea5e7ae9be854b
Alexlender <sashacharugin@mail.ru>
2024-05-02 18:15:25 +0700

    views

--- choice.php
+++ choice.php
@@ -7,9 +7,6 @@ $selected = $_GET["value"] == 1 ? $_SESSION["items"][0] : $_SESSION["items"][1];
 $notselected = $_GET["value"] == 0 ? $_SESSION["items"][0] : $_SESSION["items"][1];
 
 $selected->wins++;
-$selected->views++;
-
-$notselected->views++;
 
 $connect->query("UPDATE `items` set `wins` = '$selected->wins', `views` = '$selected->views' where `name` = '$selected->name'");
 $connect->query("UPDATE `items` set `views` = '$notselected->views' where `name` = '$notselected->name'");
