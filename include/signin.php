1c42b2911105642aa1307fe6d6877e02b25d6c00
samfoxx <foxikmain@gmail.com>
2024-05-02 15:46:34 +0700

    passwd

--- include/signin.php
+++ include/signin.php
@@ -9,7 +9,7 @@
     $login = $_POST['login'];
     $password = md5($_POST['password']);
 
-    $check_user = mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = '$login' AND `password` = '$password'");
+    $check_user = mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = '$login' AND `passwd` = '$password'");
     if (mysqli_num_rows($check_user) > 0) {
 
         $user = mysqli_fetch_assoc($check_user);
