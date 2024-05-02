4c0da7a133a3cf0afe8e1d7d733cede91c0fb8b1
Alexlender <sashacharugin@mail.ru>
2024-05-02 18:05:29 +0700

    qrw

--- choice.php
+++ choice.php
@@ -4,7 +4,12 @@ require_once 'include/connect.php';
 session_start();
 
 $selected = $_GET["value"] == 1 ? $_SESSION["items"][0] : $_SESSION["items"][1];
+$notselected = $_GET["value"] == 0 ? $_SESSION["items"][0] : $_SESSION["items"][1];
 
 $selected->wins++;
+$selected->views++;
 
-$connect->query("UPDATE `items` set `wins` = '$selected->wins' where `name` = '$selected->name'");
+$notselected->views++;
+
+$connect->query("UPDATE `items` set `wins` = '$selected->wins', `views` = `$selected->views` where `name` = '$selected->name'");
+$connect->query("UPDATE `items` set `views` = `$notselected->views` where `name` = '$notselected->name'");
