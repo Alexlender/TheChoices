eb5ed70294ec7382a18b63498e0382d5a2ba51b8
Alexlender <sashacharugin@mail.ru>
2024-05-02 17:47:23 +0700

    main

--- choice.php
+++ choice.php
@@ -7,5 +7,3 @@ $selected = $_GET["value"] == 1 ? $_SESSION["items"][0] : $_SESSION["items"][1];
 require_once 'include/connect.php';
 
 $connect->query("UPDATE items set wins = $selected->wins+1 where 'name' = $selected->name");
-
-
