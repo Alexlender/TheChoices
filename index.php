b61a42042844791614b60eb27d2c06a5207c26ff
Alexlender <sashacharugin@mail.ru>
2024-05-02 17:11:16 +0700

    choice

--- index.php
+++ index.php
@@ -31,7 +31,8 @@ if (!$_SESSION['user']) {
         require_once 'include/get_items.php';
 
         $items = get_items();
-
+        $_SESSION['items'] = $items;
+        
         echo '<div class="compare-container">
     <img src="/template/images/' . $items[0]->image . '" alt="линукс???">
     <div id="span-compare">' . $items[0]->name . '</div>
