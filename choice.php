0b72fc68bc53f6b7e08eb274a0595f9f5f3b87c5
Alexlender <sashacharugin@mail.ru>
2024-05-02 17:42:02 +0700

    работай

--- choice.php
+++ choice.php
@@ -1,3 +1,11 @@
 <?php
+
 session_start();
-echo print_r($_SESSION["items"][0]);
+
+$selected = $_GET["value"] == 1 ? $_SESSION["items"][0] : $_SESSION["items"][1];
+
+require_once 'include/connect.php';
+
+$connect->query("UPDATE items set wins = $selected->wins+1 where name = $selected->name");
+
+
