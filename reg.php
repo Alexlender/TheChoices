14ce548923a90e3f4f5f7a18a1d5df3892ea2fb8
samfoxx <foxikmain@gmail.com>
2024-05-02 11:07:06 +0700

    коммит №1473983249723947

--- reg.php
+++ reg.php
@@ -1,42 +1,53 @@
 <?php
-	session_start();
+session_start();
 
-	if ($_SESSION['user']) {
-        header('Location: ../profile.php');
-    }
+if ($_SESSION['user']) {
+    header('Location: ../profile.php');
+}
 ?>
 
 <html>
-<head>
-	<title>ЧТО ЛУЧШЕ??????</title>
-	<metacharset=”utf-8”>
-	<link href="style_form.css" rel="stylesheet">
-</head>
-<body>
-	<div class="auth__form">
-		<form action="include/signup.php" method="post" enctype="multipart/form-data">
-			<label>Фамилия и имя</label>
-			<input type="text" name="full_name" placeholder="Введите фамилию и имя">
-			<label>Логин</label>
-			<input type="text" name="login" placeholder="Введите логин">
-			<label>Почта</label>
-			<input type="email" name="email" placeholder="Введите свою электронную почту">
-			<label>Пароль</label>
-			<input type="password" name="password" placeholder="Введите пароль">
-			<label>Подтверждение пароля</label>
-			<input type="password" name="password_confirm" placeholder="Подтвердите пароль">
-			<button type="submit">Зарегистрироваться</button>
-			<p>
-				<a href="/auth.php">Авторизация</a>
-			</p>
 
-			<?php
-				if ($_SESSION['message']) {
-					echo '<script>alert('. $_SESSION['message'].')</script>';
-				}
-				unset($_SESSION['message']);
-			?> 
-		</form>
-	</div>
+<head>
+    <title>ЧТО ЛУЧШЕ??????</title>
+    <metacharset=”utf-8”>
+        <link href="style_form.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="auth__form">
+        <form action="include/signup.php" method="post" enctype="multipart/form-data">
+            <label>Фамилия и имя</label>
+            <input type="text" name="full_name" placeholder="Введите фамилию и имя">
+            <label>Логин</label>
+            <input type="text" name="login" placeholder="Введите логин">
+            <label>Почта</label>
+            <input type="email" name="email" placeholder="Введите свою электронную почту">
+            <label>Пароль</label>
+            <input type="password" name="password" placeholder="Введите пароль">
+            <label>Подтверждение пароля</label>
+            <input type="password" name="password_confirm" placeholder="Подтвердите пароль">
+            <button type="submit">Зарегистрироваться</button>
+            <p>
+                <a href="/auth.php">Авторизация</a>
+            </p>
+
+            <?php
+            if ($_SESSION['message']) {
+                died($_SESSION['message']);
+            }
+            unset($_SESSION['message']);
+            ?>
+        </form>
+    </div>
 </body>
+
 </html>
+
+<?php
+            function died($error)
+            { ?>
+                <script>alert("<?php echo $error; ?>")</script>
+
+                <?php die();
+            } ?>
