9feae135f1c92a9ad61ff9405afbf0c3296cc50b
Alexlender <sashacharugin@mail.ru>
2024-05-02 16:20:34 +0700

    ;

--- include/get_items.php
+++ include/get_items.php
@@ -5,7 +5,7 @@ require_once 'connect.php';
 $result = $connect->query("SELECT * FROM items order by RAND() LIMIT 2");
 
 while ($row = $result->fetch_assoc()) {
-    echo print_r($row["name"])
+    echo print_r($row["name"]);
 }
 
 ?>
