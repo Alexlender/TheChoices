d415434138c59ea0c4cd1eea5029bd6d2b305595
samfoxx <foxikmain@gmail.com>
2024-05-02 16:08:05 +0700

    коммиттттт

---
+++ template/header.php
@@ -0,0 +1,11 @@
+<header>
+    <div class="header">
+        <img src="/template/images/logo.gif">
+        <div class="nav-container">
+            <nav>
+                <a class="nav_link" href="#">Главная</a>
+                <a class="nav_link" href="#">Форум</a>
+            </nav>
+        </div>
+    </div>
+</header>
