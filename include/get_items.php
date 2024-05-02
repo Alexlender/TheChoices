cd8dac2b69354424dc2f990a871b933c7bc959e2
Alexlender <sashacharugin@mail.ru>
2024-05-02 16:32:24 +0700

    1234

--- include/get_items.php
+++ include/get_items.php
@@ -7,12 +7,12 @@ $result = $connect->query("SELECT * FROM items order by RAND() LIMIT 2");
 
 $row = $result->fetch_assoc();
 $item1 = new item();
-$item1->$name = $row["name"]
-$item1->$image = $row["image"]
+$item1->$name = $row["name"];
+$item1->$image = $row["image"];
 
 $row = $result->fetch_assoc();
 $item2 = new item();
-$item2->$name = $row["name"]
-$item2->$image = $row["image"]
+$item2->$name = $row["name"];
+$item2->$image = $row["image"];
 
 ?>
