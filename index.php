2127a808d128ba3dbd908a8d1708c4f84554edaf
Alexlender <sashacharugin@mail.ru>
2024-05-02 17:26:21 +0700

    id

--- index.php
+++ index.php
@@ -32,14 +32,14 @@ if (!$_SESSION['user']) {
 
         $items = get_items();
 
-        echo '<div class="compare-container">
+        echo '<div class="compare-container" id="1">
     <img src="/template/images/' . $items[0]->image . '" alt="линукс???">
     <div id="span-compare">' . $items[0]->name . '</div>
     </div>';
 
         echo '<img src="/template/images/or.gif" alt="или" width="70px" height="50px">';
 
-        echo '<div class="compare-container">
+        echo '<div class="compare-container" id="2">
     <img src="/template/images/' . $items[1]->image . '" alt="линукс???">
     <div id="span-compare">' . $items[1]->name . '</div>
     </div>';
--- scripts/jq.js
+++ scripts/jq.js
@@ -1,6 +1,6 @@
 $("div.compare-container").on("click", function (obj) {
 
-    console.log(obj.name)
+    console.log(obj.id)
 
 
 });
