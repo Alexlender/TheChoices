352b989117e409c4719d2d2f08a35daaf6813fd8
Alexlender <sashacharugin@mail.ru>
2024-05-02 18:18:55 +0700

    relod

--- scripts/jq.js
+++ scripts/jq.js
@@ -1,5 +1,5 @@
 $("div.compare-container").on("click", function (obj) {
 
     $.get('/choice.php', {value : obj['currentTarget']["id"]})
-    window.location.href("index.php")
+    location.reload()
 });
