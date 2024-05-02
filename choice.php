af7e2f69322df3fff592f7a09fc76fd7e15994d4
Alexlender <sashacharugin@mail.ru>
2024-05-02 17:51:14 +0700

    nameee

--- choice.php
+++ choice.php
@@ -7,4 +7,4 @@ $selected = $_GET["value"] == 1 ? $_SESSION["items"][0] : $_SESSION["items"][1];
 
 
 
-$connect->query("UPDATE items set wins = $selected->wins+1 where 'name' = $selected->name");
+$connect->query("UPDATE 'items' set 'wins' = $selected->wins+1 where 'name' = $selected->name");
