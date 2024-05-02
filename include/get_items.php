7be9830d043d36c6fb7fe40ded8808b094d34491
Alexlender <sashacharugin@mail.ru>
2024-05-02 16:11:11 +0700

    asd

--- include/get_items.php
+++ include/get_items.php
@@ -1,12 +1,12 @@
 <?php
-session_start();
+
 require_once 'connect.php';
 
 $result = $connect->query("SELECT * FROM items order by RAND() LIMIT 2");
 
 while($row = mysql_fetch_array($result)) {
-    echo $row['column_name']; // Print a single column data
-    echo print_r($row);       // Print the entire row data
+    echo $row['column_name'];
+    echo print_r($row);       
 }
 
 ?>
