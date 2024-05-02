53f1fd07d85c14dafa4d69c74ca537cc26799c3f
Alexlender <sashacharugin@mail.ru>
2024-05-02 16:45:41 +0700

    еее

--- include/get_items.php
+++ include/get_items.php
@@ -1,17 +1,22 @@
 <?php
+function get_items()
+{
+    require_once 'connect.php';
 
-require_once 'connect.php';
-
-$result = $connect->query("SELECT * FROM items order by RAND() LIMIT 2");
+    $result = $connect->query("SELECT * FROM items order by RAND() LIMIT 2");
 
 
-$row = $result->fetch_assoc();
-$item1 = new item();
-$item1->$name = $row["name"];
-$item1->$image = $row["image"];
+    $row = $result->fetch_assoc();
+    $item1 = new item();
+    $item1->$name = $row["name"];
+    $item1->$image = $row["image"];
 
-$row = $result->fetch_assoc();
-$item2 = new item();
-$item2->$name = $row["name"];
-$item2->$image = $row["image"];
+    $row = $result->fetch_assoc();
+    $item2 = new item();
+    $item2->$name = $row["name"];
+    $item2->$image = $row["image"];
+
+    return array($item1, $item2);
+}
+
 
