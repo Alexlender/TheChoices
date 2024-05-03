2367fa92946cdeb31450b7a826c5d06f6114ea69
Alexlender <sashacharugin@mail.ru>
2024-05-03 19:17:10 +0700

    ignore

---
+++ upload_image.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'include/connect.php';
+session_start();
+
+$file = $_FILES["image"]['tmp_name'];
+
+$connect->query("LOAD XML LOCAL INFILE '$file' INTO TABLE items;");
+
+
+header("Location: /adminpage.php");
+
+
+
+?>
