52e4ffca1b7c1f8132131fe63ffc413946c845e8
Alexlender <sashacharugin@mail.ru>
2024-05-04 01:32:50 +0700

    ышпт

--- template/header.php
+++ template/header.php
@@ -3,14 +3,21 @@
         <img src="/template/images/logo.gif">
         <div class="nav-container">
             <nav>
-                <a class="nav_link" href="../index.php">Главная</a>
+                <a class="nav_link" href="../">Главная</a>
             </nav>
         </div>
     </div>
     <?php
     if ($_SESSION['user']['isAdmin'] == 1){
-        echo "<a href='../adminpage.php'>Админ панель</a>";
+        echo "<a href='../adminpage'>Админ панель</a>";
+    }
+
+    if ($_SESSION['user']){
+        echo "<a href='../user/signout'>Выйти</a>";
+    }
+    else{
+        echo "<a href='../user/signin'>Войти</a>";
     }
     ?>
-    <a href="../user/signout.php">Выйти</a>
+
 </header>
