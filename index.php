ec8d8b1e31bdb485b77cfd473f59630efd63e0bc
Alexlender <sashacharugin@mail.ru>
2024-05-03 19:11:12 +0700

    safsaf

--- index.php
+++ index.php
@@ -21,16 +21,6 @@ if (!$_SESSION['user']) {
 
     <h1 class="first_text">Что лучше?</h1>
 
-    <?php
-// The message
-$message = "Line 1\r\nLine 2\r\nLine 3";
-
-// In case any of our lines are larger than 70 characters, we should use wordwrap()
-$message = wordwrap($message, 70, "\r\n");
-
-// Send
-echo print_r(mail('caffeinated@example.com', 'My Subject', $message));
-?>
 
 
 
