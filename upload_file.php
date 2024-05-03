3d66119c79d18889890328b0cdf43352d4fb0573
Alexlender <sashacharugin@mail.ru>
2024-05-03 15:44:13 +0700

    post

--- upload_file.php
+++ upload_file.php
@@ -3,9 +3,11 @@ session_start();
 require_once 'include/connect.php';
 
 $postData = file_get_contents('php://input');
+echo print_r($postData);
+
 $xml = simplexml_load_string($postData);
 // $xml = $_POST['xml'];
 
-echo print_r($xml);
+// echo print_r($xml);
 
 ?>
