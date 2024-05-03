045fc6ea02b7f12ba6aad62753bc7002004f13c9
Alexlender <sashacharugin@mail.ru>
2024-05-03 19:08:27 +0700

    adminpage

--- upload_file.php
+++ upload_file.php
@@ -3,17 +3,12 @@
 require_once 'include/connect.php';
 session_start();
 
-
-$postData = simplexml_load_string(file_get_contents($_FILES["xml"]['tmp_name']));
-echo print_r($postData);
-
 $file = $_FILES["xml"]['tmp_name'];
 
-$result = $connect->query("LOAD XML LOCAL INFILE '$file' INTO TABLE items;");
-
-echo $result;
+$connect->query("LOAD XML LOCAL INFILE '$file' INTO TABLE items;");
 
 
+header("Location: /adminpage.php");
 
 
 
