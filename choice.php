7d8aa803bbcecb01b2ae9eec7fd475caff180fec
Alexlender <sashacharugin@mail.ru>
2024-05-04 01:22:10 +0700

    fix

--- choice.php
+++ choice.php
@@ -1,12 +1,7 @@
 <?php
 
-session_start();
-
-if ($_SESSION['user']['isAdmin'] != 1) {
-    header('Location: /index.php');
-}
-
 require_once 'include/connect.php';
+session_start();
 
 $selected = $_GET["value"] == 1 ? $_SESSION["items"][0] : $_SESSION["items"][1];
 $notselected = $_GET["value"] == 0 ? $_SESSION["items"][0] : $_SESSION["items"][1];
