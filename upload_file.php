eb66e50d19c3ee960cf3fde27cf37ca97ca0b762
Alexlender <sashacharugin@mail.ru>
2024-05-03 17:11:00 +0700

    asd

--- upload_file.php
+++ upload_file.php
@@ -7,9 +7,10 @@ require_once 'include/connect.php';
 
 
 
-// $data = file_get_contents($_FILES); 
-// $xml = $_POST['xml'];
+$data = file_get_contents($_FILES["xml"]); 
+echo print_r($data);
+//$xml = $_POST['xml'];
 
-echo print_r(simplexml_load_string(file_get_contents('php://xml')));
+// echo print_r(simplexml_load_string(file_get_contents('php://xml')));
 
 ?>
