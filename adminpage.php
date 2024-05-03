12e9052e85e820bf4ebc56c7bc553783a482c914
Alexlender <sashacharugin@mail.ru>
2024-05-03 20:05:32 +0700

    divs

--- adminpage.php
+++ adminpage.php
@@ -19,27 +19,58 @@ if ($_SESSION['user']['isAdmin'] != 1) {
 
     <?php include 'template/header.php'; ?>
 
-    <div class="mega-cont">
-        <div class="mini-cont"
-            style="margin:auto; width:fit-content; display: grid; grid-template-columns:1fr 1fr 1fr 1fr 1fr;  gap: 30px;">
-            <form method="GET" action="adminpage.php">
+    <div class="super-mega-cont">
+        <div class="mega-cont">
+            <div class="mini-cont"
+                style="margin:auto; width:fit-content; display: grid; grid-template-columns:1fr 1fr 1fr 1fr 1fr;  gap: 30px;">
+                <form method="GET" action="adminpage.php">
 
-                <select name="sort">
-                    <option value="" disabled selected>Сортировака</option>
-                    <!-- <option value="poor">Сначала недорогие</option> -->
-                    <option value="price">Сначала дорогие</option>
-                    <option value="winrate">Сначала крутые</option>
-                    <option value="name">Сначала на букву "А"</option>
-                </select>
-                <input type='submit' value="Показать" />
+                    <select name="sort">
+                        <option value="" disabled selected>Сортировака</option>
+                        <!-- <option value="poor">Сначала недорогие</option> -->
+                        <option value="price">Сначала дорогие</option>
+                        <option value="winrate">Сначала крутые</option>
+                        <option value="name">Сначала на букву "А"</option>
+                    </select>
+                    <input type='submit' value="Показать" />
 
-            </form>
+                </form>
 
-            <form method="GET" action="adminpage.php">
-                <input name='search' type="text" placeholder="поиск" style="font-size:20px; width:80%; margin:auto" />
-                <input type="submit" value="Найти" />
-            </form>
+                <form method="GET" action="adminpage.php">
+                    <input name='search' type="text" placeholder="поиск"
+                        style="font-size:20px; width:80%; margin:auto" />
+                    <input type="submit" value="Найти" />
+                </form>
 
+            </div>
+            <?php
+            require_once 'include/get_items.php';
+
+            $items = get_all_items($_GET['sort'], $_GET['search']);
+            $counter = 1;
+            foreach ($items as &$item) {
+
+                echo "
+        <div class='cont-item'>
+        <h1>$counter</h1>
+        <img src='/template/images/$item->image' alt='$item->image'></img>
+        <div style='display:grid; grid-template-rows:1fr 1fr;'> 
+        <p>
+                Винрейт $item->name $item->winrate%
+        </p>
+        <p>
+            Стоимость $item->price рублей
+        </p>
+        </div>
+        </div>";
+                $counter++;
+            }
+
+
+            ?>
+        </div>
+
+        <div class=forms>
 
             <div style="width:50%;">
                 <form method="POST" enctype="multipart/form-data" action="upload_file.php"
@@ -64,31 +95,6 @@ if ($_SESSION['user']['isAdmin'] != 1) {
                 <input type="submit" value="Сбросить вообще всё" />
             </form>
         </div>
-        <?php
-        require_once 'include/get_items.php';
-
-        $items = get_all_items($_GET['sort'], $_GET['search']);
-        $counter = 1;
-        foreach ($items as &$item) {
-
-            echo "
-        <div class='cont-item'>
-        <h1>$counter</h1>
-        <img src='/template/images/$item->image' alt='$item->image'></img>
-        <div style='display:grid; grid-template-rows:1fr 1fr;'> 
-        <p>
-                Винрейт $item->name $item->winrate%
-        </p>
-        <p>
-            Стоимость $item->price рублей
-        </p>
-        </div>
-        </div>";
-            $counter++;
-        }
-
-
-        ?>
     </div>
 
 </body>
