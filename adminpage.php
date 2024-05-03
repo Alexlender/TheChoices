78076efe3eb031ce9408e518b83579931408e362
samfoxx <foxikmain@gmail.com>
2024-05-03 14:36:59 +0700

    div

--- adminpage.php
+++ adminpage.php
@@ -20,33 +20,37 @@ if ($_SESSION['user']['isAdmin'] != 1) {
     <?php include 'template/header.php'; ?>
 
     <div class="mega-cont">
-    <div class="mini-cont" style="margin:auto; width:fit-content; display: grid; grid-template-columns:2fr 1fr;  gap: 20px;">
-    <form method="GET" action="adminpage.php">
+        <div class="mini-cont"
+            style="margin:auto; width:fit-content; display: grid; grid-template-columns:1fr 1fr 1fr;  gap: 20px;">
+            <form method="GET" action="adminpage.php">
 
-        <select name="sort">
-            <option value="" disabled selected>Сортировака</option>
-            <!-- <option value="poor">Сначала недорогие</option> -->
-            <option value="price">Сначала дорогие</option>
-            <option value="winrate">Сначала крутые</option>
-            <option value="name">Сначала на букву "А"</option>
-        </select>
-        <input type='submit' value="Показать"/>
+                <select name="sort">
+                    <option value="" disabled selected>Сортировака</option>
+                    <!-- <option value="poor">Сначала недорогие</option> -->
+                    <option value="price">Сначала дорогие</option>
+                    <option value="winrate">Сначала крутые</option>
+                    <option value="name">Сначала на букву "А"</option>
+                </select>
+                <input type='submit' value="Показать" />
 
-    </form>
+            </form>
 
-    
-    <input type="text" placeholder="поиск" style="font-size:20px; width:80%; margin:auto"/>
-    <input type="file"/>
-    </div>
-    <?php
-    require_once 'include/get_items.php';
 
-    $items = get_all_items($_GET['sort']);
-    $counter = 1;
-   // echo $items[0];
-    foreach ($items as &$item) {
+            <input type="text" placeholder="поиск" style="font-size:20px; width:80%; margin:auto" />
+            <div>
+                <label>Загрузить XML-файл новой штуки</label>
+                <input type="file" accept="text/xml" />
+            </div>
+        </div>
+        <?php
+        require_once 'include/get_items.php';
 
-        echo"
+        $items = get_all_items($_GET['sort']);
+        $counter = 1;
+        // echo $items[0];
+        foreach ($items as &$item) {
+
+            echo "
         <div class='cont-item'>
         <h1>$counter</h1>
         <img src='/template/images/$item->image'></img>
@@ -59,11 +63,11 @@ if ($_SESSION['user']['isAdmin'] != 1) {
         </p>
         </div>
         </div>";
-        $counter++;
-    }
+            $counter++;
+        }
 
 
-    ?>
+        ?>
     </div>
 
 </body>
