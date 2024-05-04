535c5deb523439c3edc0be62f9c265a8180f53ce
Alexlender <sashacharugin@mail.ru>
2024-05-04 11:04:28 +0700

    500

--- error/500.php
+++ error/500.php
@@ -1,2 +1,20 @@
-<?php
-echo "<h>Всё сломалось :(</h>";
+<html>
+
+<head>
+    <title>Что лучше??</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <?php include 'template/header.php'; ?>
+
+
+    <?php
+    echo "<h>Всё сломалось :(</h>";
+    ?>
+
+</body>
+
+
+</html>
