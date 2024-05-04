4a7dc7421b6b5236563d35604c1798e0d0b687ee
Alexlender <sashacharugin@mail.ru>
2024-05-04 17:33:18 +0700

    prod

--- choice.php
+++ choice.php
@@ -4,18 +4,18 @@ require_once 'include/connect.php';
 session_start();
 
 $selected = $_GET["value"] == 1 ? $_SESSION["items"][0] : $_SESSION["items"][1];
-$notselected = $_GET["value"] == 0 ? $_SESSION["items"][0] : $_SESSION["items"][1];
+$notselected = $_GET["value"] == 2 ? $_SESSION["items"][0] : $_SESSION["items"][1];
 
 
-echo '<div class="compare-container" id="1">
-<img src="/template/images/' . $selected->image . '" alt="' . $selected->name . '">
-<div id="span-compare">' . $selected->name . '</div>
-</div>';
+// echo '<div class="compare-container" id="1">
+// <img src="/template/images/' . $selected->image . '" alt="' . $selected->name . '">
+// <div id="span-compare">' . $selected->name . '</div>
+// </div>';
 
-echo '<div class="compare-container" id="2">
-<img src="/template/images/' . $notselected->image . '" alt="' . $notselected->name . '">
-<div id="span-compare">' . $notselected->name . '</div>
-</div>';
+// echo '<div class="compare-container" id="2">
+// <img src="/template/images/' . $notselected->image . '" alt="' . $notselected->name . '">
+// <div id="span-compare">' . $notselected->name . '</div>
+// </div>';
 
 $connect->query("UPDATE `items` set `wins` = `wins` + 1, `views` = `views` + 1 where `name` = '$selected->name'");
 $connect->query("UPDATE `items` set `views` = `views` + 1 where `name` = '$notselected->name'");
