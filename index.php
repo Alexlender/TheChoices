9af0a320e8ae1db392e6a8a9e54a08d9e003c6f4
Alexlender <sashacharugin@mail.ru>
2024-05-06 12:34:39 +0700

    fix

--- index.php
+++ index.php
@@ -26,20 +26,20 @@ if (!$_SESSION['user']) {
         <?php
         require_once 'include/get_items.php';
 
-
-        $items = get_items();
-        $_SESSION['items'] = $items;
-
+        if(!$_SESSION['items']){
+            $_SESSION['items'] = get_items();
+        }
+        $items = $_SESSION['items'];
 
         echo '<div class="compare-container" id="1">
-    <img src="/template/images/' . $items[0]->image . '" alt="' . $items[0]->name . '">
+    <img src="/template/images/' . $items[0]->image . '" alt="'.$items[0]->name.'">
     <div id="span-compare">' . $items[0]->name . '</div>
     </div>';
 
         echo '<img src="/template/images/or.gif" alt="или" width="70px" height="50px">';
 
         echo '<div class="compare-container" id="2">
-    <img src="/template/images/' . $items[1]->image . '" alt="' . $items[1]->name . '">
+    <img src="/template/images/' . $items[1]->image . '" alt="'.$items[1]->name.'">
     <div id="span-compare">' . $items[1]->name . '</div>
     </div>';
 
